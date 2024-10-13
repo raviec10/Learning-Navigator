0a31e5d871d9b8d92fd0357a837d6ce05a1e2da7
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-13 20:12:53 +0530

    Update SubjectController.java

--- src/main/controller/SubjectController.java
+++ src/main/controller/SubjectController.java
@@ -1,1 +1,26 @@
+@RestController
+@RequestMapping("/subjects")
+public class SubjectController {
+    @Autowired
+    private SubjectService subjectService;
 
+    @GetMapping
+    public List<Subject> getAllSubjects() {
+        return subjectService.getAllSubjects();
+    }
+
+    @GetMapping("/{id}")
+    public Subject getSubjectById(@PathVariable Long id) {
+        return subjectService.getSubjectById(id);
+    }
+
+    @PostMapping
+    public Subject createSubject(@RequestBody Subject subject) {
+        return subjectService.saveSubject(subject);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteSubject(@PathVariable Long id) {
+        subjectService.deleteSubject(id);
+    }
+}
