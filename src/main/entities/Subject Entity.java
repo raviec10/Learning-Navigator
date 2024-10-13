6776249de419ab74fbc61a417abea9f0601575ce
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-13 20:03:52 +0530

    Update Subject Entity.java

--- src/main/entities/Subject Entity.java
+++ src/main/entities/Subject Entity.java
@@ -1,1 +1,35 @@
+@Entity
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long subjectId;
+    private String subjectName;
 
+    @ManyToMany(mappedBy = "enrolledSubjects")
+    private List<Student> students = new ArrayList<>();
+
+    // Getters and Setters
+    public Long getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(Long subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
