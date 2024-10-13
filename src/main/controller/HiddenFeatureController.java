99d5426fcefbd081ba6f9211296e16f607b88191
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-13 20:11:37 +0530

    Update HiddenFeatureController.java

--- src/main/controller/HiddenFeatureController.java
+++ src/main/controller/HiddenFeatureController.java
@@ -1,1 +1,11 @@
+@RestController
+@RequestMapping("/hidden-feature")
+public class EasterEggController {
 
+    @GetMapping("/{number}")
+    public ResponseEntity<String> getNumberFact(@PathVariable int number) {
+        // Use a Numbers API client or external library to get a fact about the number
+        String fact = "Number " + number + " is interesting!";
+        return ResponseEntity.ok(fact);
+    }
+}
