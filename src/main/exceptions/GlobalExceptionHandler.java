342e6ebf5d72ee084f89e3d9561bdee636a0152b
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-13 20:11:51 +0530

    Update GlobalExceptionHandler.java

--- src/main/exceptions/GlobalExceptionHandler.java
+++ src/main/exceptions/GlobalExceptionHandler.java
@@ -1,1 +1,9 @@
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 
+    // Other exception handling methods can be added here
+}
