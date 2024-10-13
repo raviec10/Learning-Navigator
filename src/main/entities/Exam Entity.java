8ad09c9f8e302581798e1d2a1a77c91f3e2c6be9
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-13 20:04:16 +0530

    Update Exam Entity.java

--- src/main/entities/Exam Entity.java
+++ src/main/entities/Exam Entity.java
@@ -1,1 +1,38 @@
+@Entity
+public class Exam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long examId;
+    
+    @ManyToOne
+    @JoinColumn(name = "subject_id")
+    private Subject subject;
 
+    @ManyToMany(mappedBy = "registeredExams")
+    private List<Student> students = new ArrayList<>();
+
+    // Getters and Setters
+    public Long getExamId() {
+        return examId;
+    }
+
+    public void setExamId(Long examId) {
+        this.examId = examId;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
