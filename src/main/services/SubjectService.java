9dd77bd77f10b2fa4da2252877b12ad9166c7372
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-13 20:06:54 +0530

    Update SubjectService.java

--- src/main/services/SubjectService.java
+++ src/main/services/SubjectService.java
@@ -1,1 +1,22 @@
+@Service
+public class SubjectService {
+    @Autowired
+    private SubjectRepository subjectRepository;
 
+    public List<Subject> getAllSubjects() {
+        return subjectRepository.findAll();
+    }
+
+    public Subject getSubjectById(Long subjectId) {
+        return subjectRepository.findById(subjectId)
+            .orElseThrow(() -> new RuntimeException("Subject not found"));
+    }
+
+    public Subject saveSubject(Subject subject) {
+        return subjectRepository.save(subject);
+    }
+
+    public void deleteSubject(Long subjectId) {
+        subjectRepository.deleteById(subjectId);
+    }
+}
