80b715ccb13e09a46a201926683a0a72f401e3a9
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-13 20:13:46 +0530

    Update StudentControllerTest.java

--- src/test/StudentControllerTest.java
+++ src/test/StudentControllerTest.java
@@ -1,1 +1,13 @@
-
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StudentControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Test
+    public void testGetStudent() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/students/1"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.name").exists());
+    }
+}
