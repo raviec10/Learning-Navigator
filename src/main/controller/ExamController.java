1d4ee5511448596aa94afd3fac6e122aa708769f
raviec10 <41552893+raviec10@users.noreply.github.com>
2024-10-13 20:08:34 +0530

    Update ExamController.java

--- src/main/controller/ExamController.java
+++ src/main/controller/ExamController.java
@@ -1,1 +1,31 @@
+@RestController
+@RequestMapping("/exams")
+public class ExamController {
+    @Autowired
+    private ExamService examService;
 
+    @GetMapping
+    public List<Exam> getAllExams() {
+        return examService.getAllExams();
+    }
+
+    @GetMapping("/{id}")
+    public Exam getExamById(@PathVariable Long id) {
+        return examService.getExamById(id);
+    }
+
+    @PostMapping
+    public Exam createExam(@RequestBody Exam exam) {
+        return examService.saveExam(exam);
+    }
+
+    @PostMapping("/{examId}/register/{studentId}")
+    public Exam registerStudentForExam(@PathVariable Long examId, @PathVariable Long studentId) {
+        return examService.registerStudentForExam(examId, studentId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteExam(@PathVariable Long id) {
+        examService.deleteExam(id);
+    }
+}
